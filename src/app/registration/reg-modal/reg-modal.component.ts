70b29c58536c97b95abe507a12296b9a87b05059
Hasitha Kaushan <hkraclh@gmail.com>
2018-07-19 10:56:18 +0530

    Add existing file

--- src/app/registration/reg-modal/reg-modal.component.ts
+++ src/app/registration/reg-modal/reg-modal.component.ts
@@ -3,6 +3,7 @@ import { NgbModal, ModalDismissReasons, NgbModule, NgbDateStruct } from '@ng-boo
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Console } from '@angular/core/src/console';
 
+//Model class for user input data
 class Registration {
   constructor(
     public firstName: string = '',
@@ -20,29 +21,44 @@ class Registration {
   styleUrls: ['./reg-modal.component.css']
 })
 export class RegModalComponent implements OnInit {
+
+  //It is required for modal settings
   closeResult: string;
+
+  //It is required for input form
   inputsForm: FormGroup;
+
+  //These is required for form controls
   firstName: string;
   lastName: string;
   email: string;
   country: string;
   public dob: NgbDateStruct = null;
+
+  //It is required to maintain the country list on dropdown
   countries: string[] = ['SRI LANKA', 'US', 'UK', 'India', 'UAE'];
+
+  //It is required to validate the email
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+  //It is required to store the state of the validity of the form
   validity = false;
 
   constructor(private modalService: NgbModal, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    //Initialization of form
     this.inputsForm = this.fb.group({
       firstName: [this.firstName, [Validators.required]],
-      lastName: [this.lastName,[Validators.required]],
+      lastName: [this.lastName, [Validators.required]],
       email: [this.email, [Validators.required, Validators.pattern(this.emailPattern)]],
       country: [this.country, [Validators.required]],
       dob: [this.dob, [Validators.required]]
     });
   }
 
+  //It is required to modal settings
   open(content) {
     this.modalService.open(content).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
@@ -50,6 +66,8 @@ export class RegModalComponent implements OnInit {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
+
+  //It is required to modal settings
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
@@ -60,14 +78,18 @@ export class RegModalComponent implements OnInit {
     }
 
   }
+
+  //It is required to get the value of the selected country on dropdown
   onChangeCountry(country: string) {
-    // Assign corresponding selected country to model.)
     this.country = country;
   }
+
+  //It is required to pass data to the parent component
   @Output()
   passData: EventEmitter<Object> = new EventEmitter();
   setValues() {
     const reg: Registration = Object.assign({}, this.inputsForm.value);
+    this.inputsForm.reset();
     this.passData.emit(reg)
   }
 }
